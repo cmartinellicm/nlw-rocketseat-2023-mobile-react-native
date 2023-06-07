6f9a0a7d48df235ddb34fbdee19f0a4d4ea0008f
Cassia Martinelli <martinelli.cassia@gmail.com>
2023-06-07 09:05:36 -0300

    feat: configure project and initial screen

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, Text, View } from 'react-native';
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import {
+  BaiJamjuree_700Bold
+} from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import { styled } from 'nativewind';
+
+const StyledStripes = styled(Stripes)
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if (!hasLoadedFonts) return null
+
+  return (
+    <ImageBackground source={blurBg} className='relative flex-1 bg-gray-900 items-center' imageStyle={{ position: 'absolute', left: '-100%' }}>
+      <StyledStripes className='absolute left-2' />
+
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  );
+}
