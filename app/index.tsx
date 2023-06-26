2d761a06773b57211c98459aae724f2ac54a0603
Cassia Martinelli <martinelli.cassia@gmail.com>
2023-06-26 09:07:10 -0300

    feat: save token and navigate using expo router

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
-import { useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+import { useEffect } from 'react'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useRouter } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { styled } from 'nativewind'
+import * as SecureStore from 'expo-secure-store'
 
 import {
   useFonts,
@@ -12,11 +16,10 @@ import {
   BaiJamjuree_700Bold
 } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
@@ -27,13 +30,15 @@ const discovery = {
 }
 
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold
   })
 
-  const [request, response, signInWithGithub] = useAuthRequest(
+  const [, response, signInWithGithub] = useAuthRequest(
     {
       clientId: 'ba3a3804a3824ac71037',
       scopes: ['identity'],
@@ -42,7 +47,19 @@ export default function App() {
       }),
     },
     discovery
-  );
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('register', {
+          code,
+        })
+    
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
 
   useEffect(() => {
     // console.log(makeRedirectUri({
@@ -50,9 +67,9 @@ export default function App() {
     // }));
     
     if (response?.type === 'success') {
-      const { code } = response.params;
+      const { code } = response.params
 
-      console.log(code);
+      handleGithubOAuthCode(code)
     }
   }, [response])
 
